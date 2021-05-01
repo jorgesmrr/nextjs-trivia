a96d1282f7a315f750f94a51d17d60c9273edec3
Jorge Moreira <jorge.smrr@gmail.com>
2021-05-01 15:05:55 -0300

    Improve ranking form layout

--- components/ranking/RankingForm.tsx
+++ components/ranking/RankingForm.tsx
@@ -17,13 +17,18 @@ const RankingForm: React.FC<RankingFormProps> = ({
     return (
         <div className="text-center">
             <p className="mb-2">Choose a username to save your score:</p>
-            <div className="inline-flex mb-2">
-                <TextField
-                    type="text"
-                    value={username}
-                    onChange={setUsername}
-                />
-                <button className="btn ml-2" onClick={() => onSubmit(username)}>
+            <div className="inline-flex flex-wrap mb-2">
+                <div className="w-full mb-2 sm:w-auto sm:mb-0">
+                    <TextField
+                        type="text"
+                        value={username}
+                        onChange={setUsername}
+                    />
+                </div>
+                <button
+                    className="btn w-full sm:ml-2 sm:w-auto"
+                    onClick={() => onSubmit(username)}
+                >
                     Save
                 </button>
             </div>
