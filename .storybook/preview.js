33c51e26e99994dfe9bffbbb2dd6ec841f320376
Jorge Moreira <jorge.smrr@gmail.com>
2021-05-29 18:34:07 -0300

    Fix images in Storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,13 @@
 import "@bit/jorgemoreira.headless.styles";
 import "../styles/globals.css";
 
+import * as nextImage from "next/image";
+
+Object.defineProperty(nextImage, "default", {
+    configurable: true,
+    value: (props) => <img {...props} />,
+});
+
 export const parameters = {
     actions: { argTypesRegex: "^on[A-Z].*" },
 };
